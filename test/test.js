f446f40dee7ca6644e789a6eb168245bd9b9d81f
Philipp Kübler <info@pkuebler.de>
2015-12-26 19:22:36 +0100

    change test framework to mocha.

--- test/test.js
+++ test/test.js
@@ -1,4 +1,3 @@
-var test   = require('utest');
 var assert = require('assert');
 var crypto = require('crypto');
 var os = require('os');
@@ -20,10 +19,10 @@ function randomString(howMany, chars) {
     return value.join('');
 }
 
-test('ttl', {
+var self = {ttl: null};
 
-	'Set Options': function() {
-
+describe('ttl', function() {
+	it('Set Options', function() {
 		var options = {
 			lastUsage: true,
 			ttl: randomString(5,'ABCDEF'),
@@ -37,172 +36,171 @@ test('ttl', {
 		assert.equal(ttl.getOptions().ttl, 0);
 		assert.equal(ttl.getOptions().checkPeriode, 0);
 		assert.equal(ttl.getOptions().timeOutFunction, null);
-	}
+	});
 });
 
-test('Push', {
+describe('Push', function() {
+	before(function() {
+		self.ttl = new timeToLife();
+	});
+	after(function() {
+		self.ttl = null;
+	});
 
-	before: function() {
-		this.ttl = new timeToLife();
-	},
-	after: function() {
-		this.ttl = null;
-	},
+	it('No Parameters', function() {
+		assert.ifError(self.ttl.push('myKey'));
+		assert.ifError(self.ttl._store.hasOwnProperty('myKey'));
+	});
 
-	'No Parameters': function() {
-		assert.ifError(this.ttl.push('myKey'));
-		assert.ifError(this.ttl._store.hasOwnProperty('myKey'));
-	},
+	it('Content', function() {
+		self.ttl.push('myKey', {name: 'Test Content'});
 
-	'Content': function() {
-		this.ttl.push('myKey', {name: 'Test Content'});
+		assert.ifError(!self.ttl._store.hasOwnProperty('myKey'));
+		assert.equal(self.ttl._store['myKey'].value.name, 'Test Content');
 
-		assert.ifError(!this.ttl._store.hasOwnProperty('myKey'));
-		assert.equal(this.ttl._store['myKey'].value.name, 'Test Content');
+		delete self.ttl._store['myKey'];
+	});
 
-		delete this.ttl._store['myKey'];
-	},
-
-	'Function Content': function() {
-		this.ttl.push('myKey', function() {
+	it('Function Content', function() {
+		self.ttl.push('myKey', function() {
 			return 'Test Content';
 		});
 
-		assert.equal(this.ttl._store['myKey'].value(), 'Test Content');
+		assert.equal(self.ttl._store['myKey'].value(), 'Test Content');
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'TimeOutFunction': function() {
-		this.ttl.push('myKey', null, function(container) {
+	it('TimeOutFunction', function() {
+		self.ttl.push('myKey', null, function(container) {
 			container['value'] = 'test';
 		});
 
-		this.ttl._store['myKey'].timeOutFunction(this.ttl._store['myKey']);
+		self.ttl._store['myKey'].timeOutFunction(self.ttl._store['myKey']);
 		
-		assert.equal(this.ttl._store['myKey'].value, "test");
+		assert.equal(self.ttl._store['myKey'].value, "test");
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'Content & TimeOutFunction': function() {
-		this.ttl.push('myKey', 'Test Content', function(container) {
+	it('Content & TimeOutFunction', function() {
+		self.ttl.push('myKey', 'Test Content', function(container) {
 			container['value'] = 'Test Content new';
 		});
 
-		assert.equal(this.ttl._store['myKey'].value, 'Test Content');
+		assert.equal(self.ttl._store['myKey'].value, 'Test Content');
 
-		this.ttl._store['myKey'].timeOutFunction(this.ttl._store['myKey']);
+		self.ttl._store['myKey'].timeOutFunction(self.ttl._store['myKey']);
 
-		assert.equal(this.ttl._store['myKey'].value, 'Test Content new');
+		assert.equal(self.ttl._store['myKey'].value, 'Test Content new');
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'Content with TTL': function() {
-		this.ttl.push('myKey', 'Test Content', null, 200);
+	it('Content with TTL', function() {
+		self.ttl.push('myKey', 'Test Content', null, 200);
 
-		assert.equal(this.ttl._store['myKey'].value, 'Test Content');
-		assert.equal(this.ttl._store['myKey'].ttl, 200);
+		assert.equal(self.ttl._store['myKey'].value, 'Test Content');
+		assert.equal(self.ttl._store['myKey'].ttl, 200);
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'TimeOutFunction with TTL': function() {
-		this.ttl.push('myKey', null, function(container) {
+	it('TimeOutFunction with TTL', function() {
+		self.ttl.push('myKey', null, function(container) {
 			container['value'] = 'Test Content new';
 		}, 200);
 
-		this.ttl._store['myKey'].timeOutFunction(this.ttl._store['myKey']);
+		self.ttl._store['myKey'].timeOutFunction(self.ttl._store['myKey']);
 
-		assert.equal(this.ttl._store['myKey'].value, 'Test Content new');
-		assert.equal(this.ttl._store['myKey'].ttl, 200);
+		assert.equal(self.ttl._store['myKey'].value, 'Test Content new');
+		assert.equal(self.ttl._store['myKey'].ttl, 200);
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
 });
 
-test('Push with Option', {
-
-	before: function() {
-		this.ttl = new timeToLife({
+describe('Push with Option', function() {
+	before(function() {
+		self.ttl = new timeToLife({
 			ttl: 200,
 			timeOutFunction: function(container) {
 				container['value'] = 'Test Content';
 			}
 		});
-	},
-	after: function() {
-		this.ttl = null;
-	},
+	});
+	after(function() {
+		self.ttl = null;
+	});
 
-	'Method': function() {
-		assert.ifError(!this.ttl.push('myKey'));
+	it('Method', function() {
+		assert.ifError(!self.ttl.push('myKey'));
 
-		this.ttl._store['myKey'].timeOutFunction(this.ttl._store['myKey']);
+		self.ttl._store['myKey'].timeOutFunction(self.ttl._store['myKey']);
 
-		assert.equal(this.ttl._store['myKey'].value, 'Test Content');
+		assert.equal(self.ttl._store['myKey'].value, 'Test Content');
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'TTL': function() {
-		this.ttl.push('myKey', 'test');
-		assert.equal(this.ttl._store['myKey'].ttl, 200);
+	it('TTL', function() {
+		self.ttl.push('myKey', 'test');
+		assert.equal(self.ttl._store['myKey'].ttl, 200);
 
-		this.ttl.push('myKey2', 'test', null, 0);
-		assert.equal(this.ttl._store['myKey2'].ttl, 0);
+		self.ttl.push('myKey2', 'test', null, 0);
+		assert.equal(self.ttl._store['myKey2'].ttl, 0);
 
-		delete this.ttl._store['myKey'];
-		delete this.ttl._store['myKey2'];
-	}
+		delete self.ttl._store['myKey'];
+		delete self.ttl._store['myKey2'];
+	});
 });
 
-test('Get', {
+describe('Get', function() {
 
-	before: function() {
-		this.ttl = new timeToLife();
-	},
-	after: function() {
-		this.ttl = null;
-	},
+	before(function() {
+		self.ttl = new timeToLife();
+	});
+	after(function() {
+		self.ttl = null;
+	});
 
-	'Content': function() {
-		this.ttl.push('myKey', 'Test Content');
+	it('Content', function() {
+		self.ttl.push('myKey', 'Test Content');
 
-		assert.equal(this.ttl.get('myKey'), 'Test Content');
+		assert.equal(self.ttl.get('myKey'), 'Test Content');
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'Functional Content': function() {
-		this.ttl.push('myKey', function() {
+	it('Functional Content', function() {
+		self.ttl.push('myKey', function() {
 			return 'Test Content';
 		});
 
-		assert.equal(this.ttl.get('myKey'), 'Test Content');
+		assert.equal(self.ttl.get('myKey'), 'Test Content');
 
-		delete this.ttl._store['myKey'];
-	},
+		delete self.ttl._store['myKey'];
+	});
 
-	'Wrong Key': function() {
-		assert.ifError(this.ttl.get('myKey'));
+	it('Wrong Key', function() {
+		assert.ifError(self.ttl.get('myKey'));
 
-		delete this.ttl._store['myKey'];
-	}
+		delete self.ttl._store['myKey'];
+	});
 });
 
-test('Remove', {
+describe('Remove', function() {
 
-	before: function() {
-		this.ttl = new timeToLife();
-	},
-	after: function() {
-		this.ttl = null;
-	},
-	'Key': function() {
-		this.ttl._store.myKey = {
+	before(function() {
+		self.ttl = new timeToLife();
+	});
+	after(function() {
+		self.ttl = null;
+	});
+
+	it('Key', function() {
+		self.ttl._store.myKey = {
 			createTime: 222,
 			lastUsage: 222,
 			ttl: 200,
@@ -211,97 +209,96 @@ test('Remove', {
 			value: 'Test Content'
 		}
 
-		assert.ifError(!this.ttl.del('myKey'));
-		assert.ifError(this.ttl._store.hasOwnProperty('myKey'));
+		assert.ifError(!self.ttl.del('myKey'));
+		assert.ifError(self.ttl._store.hasOwnProperty('myKey'));
 
-		delete this.ttl._store['myKey'];
-	},
-	'Wrong Key': function() {
-		assert.equal(this.ttl.del('falseKey'), 0);
-	}
+		delete self.ttl._store['myKey'];
+	});
+	it('Wrong Key', function() {
+		assert.equal(self.ttl.del('falseKey'), 0);
+	});
 });
 
-test('Multiple', {
+describe('Multiple', function() {
 
-	before: function() {
-		this.ttl = new timeToLife();
-		this.ttl.push("key1", randomString(10));
-		this.ttl.push("key2", randomString(10));
-		this.ttl.push("key3", randomString(10));
-		this.ttl.push("key4", randomString(10));
-	},
-	after: function() {
-		this.ttl = null;
-	},
-	'Get': function() {
-		var value = this.ttl.get(["key1","key2","key4"]);
+	before(function() {
+		self.ttl = new timeToLife();
+		self.ttl.push("key1", randomString(10));
+		self.ttl.push("key2", randomString(10));
+		self.ttl.push("key3", randomString(10));
+		self.ttl.push("key4", randomString(10));
+	});
+	after(function() {
+		self.ttl = null;
+	});
+	it('Get', function() {
+		var value = self.ttl.get(["key1","key2","key4"]);
 
-		assert.equal(value.key1, this.ttl._store["key1"].value);
-		assert.equal(value.key2, this.ttl._store["key2"].value);
-		assert.notEqual(value.key3, this.ttl._store["key3"].value);
-		assert.equal(value.key4, this.ttl._store["key4"].value);
-	},
-	'Del': function() {
-		var value = this.ttl.del(["key1","key2","key4"]);
+		assert.equal(value.key1, self.ttl._store["key1"].value);
+		assert.equal(value.key2, self.ttl._store["key2"].value);
+		assert.notEqual(value.key3, self.ttl._store["key3"].value);
+		assert.equal(value.key4, self.ttl._store["key4"].value);
+	});
+	it('Del', function() {
+		var value = self.ttl.del(["key1","key2","key4"]);
 		assert.equal(value, 3);
 
-		assert.ifError(this.ttl._store.hasOwnProperty('key1'));
-		assert.ifError(!this.ttl._store.hasOwnProperty('key3'));
-		assert.ifError(this.ttl._store.hasOwnProperty('key4'));
+		assert.ifError(self.ttl._store.hasOwnProperty('key1'));
+		assert.ifError(!self.ttl._store.hasOwnProperty('key3'));
+		assert.ifError(self.ttl._store.hasOwnProperty('key4'));
 
-		delete this.ttl._store['key3'];
-	}
-
+		delete self.ttl._store['key3'];
+	});
 });
 
-test('Tools', {
-	before: function() {
-		this.ttl = new timeToLife();
-		this.ttl.push("key1", randomString(10));
-		this.ttl.push("key2", randomString(10));
-		this.ttl.push("key3", randomString(10));
-		this.ttl.push("key4", randomString(10));
-	},
-	after: function() {
-		this.ttl = null;
-	},
-	'Size': function() {
-		assert.equal(this.ttl.size(), 4);
-	},
-	'Clear': function() {
-		this.ttl.clear();
-		assert.equal(this.ttl.size(), 0);
+describe('Tools', function() {
+	before(function() {
+		self.ttl = new timeToLife();
+		self.ttl.push("key1", randomString(10));
+		self.ttl.push("key2", randomString(10));
+		self.ttl.push("key3", randomString(10));
+		self.ttl.push("key4", randomString(10));
+	});
+	after(function() {
+		self.ttl = null;
+	});
+	it('Size', function() {
+		assert.equal(self.ttl.size(), 4);
+	});
+	it('Clear', function() {
+		self.ttl.clear();
+		assert.equal(self.ttl.size(), 0);
 
-		this.ttl.push("key1", randomString(10));
-		assert.equal(this.ttl.size(), 1);
+		self.ttl.push("key1", randomString(10));
+		assert.equal(self.ttl.size(), 1);
 
-		delete this.ttl._store["key1"];
-	}
+		delete self.ttl._store["key1"];
+	});
 });
 
-test('Expired', {
-	before: function() {
-		this.ttl = new timeToLife();
-	},
-	after: function() {
-		this.ttl = null;
-	},
-	'Check': function() {
-		this.ttl.push("key1", randomString(20), null, 5);
-		this.ttl._store['key1'].createTime = this.ttl._store['key1'].createTime-1000;
-		assert.ifError(this.ttl._check(this.ttl._store["key1"]));
-		assert.ifError(this.ttl._store.hasOwnProperty('key1'));
-	},
-	'TimeOutFunction': function() {
+describe('Expired', function() {
+	before(function() {
+		self.ttl = new timeToLife();
+	});
+	after(function() {
+		self.ttl = null;
+	});
+	it('Check', function() {
+		self.ttl.push("key1", randomString(20), null, 5);
+		self.ttl._store['key1'].createTime = self.ttl._store['key1'].createTime-1000;
+		assert.ifError(self.ttl._check(self.ttl._store["key1"]));
+		assert.ifError(self.ttl._store.hasOwnProperty('key1'));
+	});
+	it('TimeOutFunction', function() {
 		var random = randomString(20);
 
-		this.ttl.push("key2", random, function(container) {
+		self.ttl.push("key2", random, function(container) {
 			container['value'] = randomString(20);
 			container['createTime'] = os.uptime();
 		}, 5);
-		this.ttl._store['key2'].createTime = this.ttl._store['key2'].createTime-1000;
-		this.ttl._check(this.ttl._store['key2']);
+		self.ttl._store['key2'].createTime = self.ttl._store['key2'].createTime-1000;
+		self.ttl._check(self.ttl._store['key2']);
 
-		assert.notEqual(random, this.ttl._store['key2'].value);
-	}
+		assert.notEqual(random, self.ttl._store['key2'].value);
+	});
 });
